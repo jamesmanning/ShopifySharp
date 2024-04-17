255dc3f0a8e86d584826e05de4d8958aa3c8a804
Joshua Harms <joshua@nozzlegear.com>
2024-04-16 22:34:49 -0500

    Remove JsonProperty and JsonPropertyName attributes
    Planning to use a camel case property name strategy with System.Text.Json instead.

--- ShopifySharp/Services/Graph/ParsedGraphResult.cs
+++ ShopifySharp/Services/Graph/ParsedGraphResult.cs
@@ -1,19 +1,13 @@
 #nullable enable
-using System;
 using System.Collections.Generic;
-using System.Text.Json.Serialization;
-using Newtonsoft.Json;
 using ShopifySharp.GraphQL;
 
 namespace ShopifySharp;
 
-[Serializable]
 public class ParsedGraphResult<T>
 {
-    [JsonProperty("data"), JsonPropertyName("data")]
     public T? Data { get; set; }
 
-    [JsonProperty("userErrors"), JsonPropertyName("userErrors")]
     public ICollection<UserError>? UserErrors { get; set; }
 
     public GraphExtensions? Extensions { get; set; }
