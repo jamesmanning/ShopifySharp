209c30932789a5eed577a2f74721a78a03b11313
Joshua Harms <joshua@nozzlegear.com>
2024-06-10 22:44:40 -0500

    Remove unused GetOptionsType interface method

--- ShopifySharp/Infrastructure/Policies/IRequestExecutionPolicyRequiresOptions.cs
+++ ShopifySharp/Infrastructure/Policies/IRequestExecutionPolicyRequiresOptions.cs
@@ -3,7 +3,4 @@ namespace ShopifySharp.Infrastructure.Policies;
 public interface IRequestExecutionPolicyRequiresOptions;
 
 public interface IRequestExecutionPolicyRequiresOptions<out TOptions> : IRequestExecutionPolicyRequiresOptions
-    where TOptions : class, IRequestExecutionPolicyOptions, new()
-{
-    static abstract Type GetOptionsType();
-}
+    where TOptions : class, IRequestExecutionPolicyOptions<TOptions>, new();
